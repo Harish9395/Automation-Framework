18e02fde48fabb68323ca0facf0e798ec704e493
Harish9395 <60750651+Harish9395@users.noreply.github.com>
2026-07-29 16:01:49 +0530

    Create driver_factory.py

---
+++ automation-framework/drivers/driver_factory.py
@@ -0,0 +1,11 @@
+from selenium import webdriver
+
+def get_driver():
+
+    options = webdriver.ChromeOptions()
+
+    driver = webdriver.Chrome(options=options)
+
+    driver.maximize_window()
+
+    return driver
