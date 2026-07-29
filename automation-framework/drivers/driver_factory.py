3eda238f84e5bef39bd1a169fb54d704685734cf
Harish9395 <60750651+Harish9395@users.noreply.github.com>
2026-07-29 17:36:32 +0530

    Update driver_factory.py

--- automation-framework/drivers/driver_factory.py
+++ automation-framework/drivers/driver_factory.py
@@ -2,56 +2,41 @@ import os
 
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.firefox.options import Options as FirefoxOptions
-from selenium.webdriver.edge.options import Options as EdgeOptions
 
 
 def get_driver():
 
-    browser = os.environ.get(
-        "BROWSER",
-        "chrome"
-    )
+    username = os.environ.get("SAUCE_USERNAME")
+    access_key = os.environ.get("SAUCE_ACCESS_KEY")
 
 
-    username = os.environ["SAUCE_USERNAME"]
-
-    access_key = os.environ["SAUCE_ACCESS_KEY"]
+    if not username or not access_key:
+        raise Exception(
+            "Sauce Labs credentials are missing"
+        )
 
 
     sauce_url = (
-        f"https://{username}:{access_key}"
-        "@ondemand.us-west-1.saucelabs.com:443/wd/hub"
+        "https://"
+        + username
+        + ":"
+        + access_key
+        + "@ondemand.us-west-1.saucelabs.com:443/wd/hub"
     )
 
 
-    if browser == "chrome":
-
-        options = Options()
+    options = Options()
 
 
-    elif browser == "firefox":
-
-        options = FirefoxOptions()
-
-
-    elif browser == "edge":
-
-        options = EdgeOptions()
-
-
-    else:
-
-        raise Exception(
-            f"Unsupported browser: {browser}"
-        )
-
+    options.set_capability(
+        "browserName",
+        "chrome"
+    )
 
     options.set_capability(
         "platformName",
         "Windows 11"
     )
-
 
     options.set_capability(
         "browserVersion",
@@ -62,8 +47,8 @@ def get_driver():
     options.set_capability(
         "sauce:options",
         {
-            "build": "Flask-ECS-Build",
-            "name": f"{browser} smoke test"
+            "name": "Flask ECS Smoke Test",
+            "build": "GitHub Actions Build"
         }
     )
 
@@ -72,9 +57,6 @@ def get_driver():
         command_executor=sauce_url,
         options=options
     )
-
-
-    driver.maximize_window()
 
 
     return driver
