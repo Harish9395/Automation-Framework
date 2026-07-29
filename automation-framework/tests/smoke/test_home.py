3808b693a76ea678e83de590896ee49eb4cf562a
Harish9395 <60750651+Harish9395@users.noreply.github.com>
2026-07-29 18:16:08 +0530

    Update test_home.py

--- automation-framework/tests/smoke/test_home.py
+++ automation-framework/tests/smoke/test_home.py
@@ -1,12 +1,8 @@
 from utilities.config import load_environment
-
+from pages.home_page import HomePage
 
 def test_home_page(driver):
-
     url = load_environment()
-
     print("Application URL:", url)
-
     home = HomePage(driver)
-
     home.open(url)
