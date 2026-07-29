cf28d2a0ace1329ca850b75f30ce7a8905c9ba12
Harish9395 <60750651+Harish9395@users.noreply.github.com>
2026-07-29 22:51:26 +0530

    Update driver_factory.py

--- automation-framework/drivers/driver_factory.py
+++ automation-framework/drivers/driver_factory.py
@@ -2,10 +2,11 @@ import os
 
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.firefox.options import Options as FirefoxOptions
+from selenium.webdriver.edge.options import Options as EdgeOptions
 
 
 def get_driver():
-
     username = os.getenv("SAUCE_USERNAME")
     access_key = os.getenv("SAUCE_ACCESS_KEY")
 
@@ -15,41 +16,40 @@ def get_driver():
     if not access_key:
         raise Exception("SAUCE_ACCESS_KEY is missing")
 
+    browser = os.getenv("BROWSER", "chrome").lower()
 
     sauce_url = "https://ondemand.eu-central-1.saucelabs.com/wd/hub"
 
+    if browser == "firefox":
+        options = FirefoxOptions()
+    elif browser == "edge":
+        options = EdgeOptions()
+    else:
+        options = Options()
 
-    options = Options()
+    options.set_capability("browserName", browser)
+    options.set_capability("browserVersion", "latest")
+    options.set_capability("platformName", "Windows 11")
 
-    options.set_capability(
-        "browserName",
-        "chrome"
-    )
+    sauce_options = {
+        "username": username,
+        "accessKey": access_key,
+        "name": os.getenv("TEST_NAME", "ECS Automation"),
+        "build": os.getenv("BUILD_NAME", "GitHub Actions"),
+        "buildId": os.getenv("BUILD_ID", ""),
+        "tags": [
+            os.getenv("GIT_BRANCH", ""),
+            "GitHub Actions"
+        ]
+    }
 
-    options.set_capability(
-        "platformName",
-        "Windows 11"
-    )
-
-    options.set_capability(
-        "browserVersion",
-        "latest"
-    )
-
-    options.set_capability(
-        "sauce:options",
-        {
-            "username": username,
-            "accessKey": access_key,
-            "name": "Flask ECS Smoke Test",
-            "build": "GitHub Actions"
-        }
-    )
-
+    options.set_capability("sauce:options", sauce_options)
 
     driver = webdriver.Remote(
         command_executor=sauce_url,
         options=options
     )
+
+    driver.maximize_window()
 
     return driver
