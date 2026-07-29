4effa9124805d0876a5b601dbf9743532a385fc5
Harish9395 <60750651+Harish9395@users.noreply.github.com>
2026-07-30 03:06:21 +0530

    Update mobile_driver.py

--- automation-framework/mobile/drivers/mobile_driver.py
+++ automation-framework/mobile/drivers/mobile_driver.py
@@ -6,40 +6,41 @@ from appium.options.android import UiAutomator2Options
 
 def get_mobile_driver():
 
-    username = os.getenv(
-        "SAUCE_USERNAME"
-    )
-
-    access_key = os.getenv(
-        "SAUCE_ACCESS_KEY"
-    )
-
+    username = os.getenv("SAUCE_USERNAME")
+    access_key = os.getenv("SAUCE_ACCESS_KEY")
+    app = os.getenv("SAUCE_APP")
 
     if not username:
-        raise Exception(
-            "SAUCE_USERNAME missing"
-        )
-
+        raise Exception("SAUCE_USERNAME is missing")
 
     if not access_key:
-        raise Exception(
-            "SAUCE_ACCESS_KEY missing"
-        )
+        raise Exception("SAUCE_ACCESS_KEY is missing")
+
+    if not app:
+        raise Exception("SAUCE_APP is missing")
 
 
     options = UiAutomator2Options()
 
 
-    options.platform_name = "Android"
+    options.set_capability(
+        "platformName",
+        "Android"
+    )
 
-    options.device_name = "Samsung Galaxy S23"
+    options.set_capability(
+        "appium:automationName",
+        "UiAutomator2"
+    )
 
-    options.automation_name = "UiAutomator2"
+    options.set_capability(
+        "appium:deviceName",
+        "Samsung Galaxy S23"
+    )
 
-
-    # Sauce Labs application
-    options.app = os.getenv(
-        "SAUCE_APP"
+    options.set_capability(
+        "appium:app",
+        app
     )
 
 
@@ -48,19 +49,40 @@ def get_mobile_driver():
         {
             "username": username,
             "accessKey": access_key,
-            "name": "ECS Mobile Test",
+
+            "name": os.getenv(
+                "TEST_NAME",
+                "ECS Mobile Test"
+            ),
+
             "build": os.getenv(
                 "BUILD_NAME",
-                "ECS-Mobile"
+                "ECS Mobile Build"
             ),
+
+            "buildId": os.getenv(
+                "BUILD_ID",
+                ""
+            ),
+
             "recordVideo": True,
-            "capturePerformance": True
+
+            "capturePerformance": True,
+
+            "tags": [
+                "mobile",
+                os.getenv(
+                    "TEST_TYPE",
+                    "Smoke"
+                )
+            ]
         }
     )
 
 
     driver = webdriver.Remote(
 
+        command_executor=
         "https://ondemand.eu-central-1.saucelabs.com/wd/hub",
 
         options=options
