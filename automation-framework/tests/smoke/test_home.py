5aec30670176a2129e01402bf3f713643fad142a
Harish9395 <60750651+Harish9395@users.noreply.github.com>
2026-07-29 18:34:09 +0530

    Update test_home.py

--- automation-framework/tests/smoke/test_home.py
+++ automation-framework/tests/smoke/test_home.py
@@ -1,8 +1,11 @@
+import pytest
 from utilities.config import load_environment
 from pages.home_page import HomePage
 
+@pytest.mark.smoke
 def test_home_page(driver):
+
     url = load_environment()
-    print("Application URL:", url)
     home = HomePage(driver)
     home.open(url)
+    assert "App Works" in driver.page_source
