eecc00efd5fa0c4e0e89e07712d3d47f2d2f057f
Harish9395 <60750651+Harish9395@users.noreply.github.com>
2026-07-29 22:54:57 +0530

    Update conftest.py

--- automation-framework/conftest.py
+++ automation-framework/conftest.py
@@ -2,11 +2,28 @@ import pytest
 
 from drivers.driver_factory import get_driver
 
+
 @pytest.fixture
-def driver():
+def driver(request):
 
     driver = get_driver()
 
     yield driver
 
+    # Update Sauce Labs test status
+    if hasattr(request.node, "rep_call"):
+        if request.node.rep_call.passed:
+            driver.execute_script("sauce:job-result=passed")
+        else:
+            driver.execute_script("sauce:job-result=failed")
+
     driver.quit()
+
+
+@pytest.hookimpl(hookwrapper=True)
+def pytest_runtest_makereport(item, call):
+    outcome = yield
+    report = outcome.get_result()
+
+    if report.when == "call":
+        item.rep_call = report
