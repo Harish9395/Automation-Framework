5297d875df8c372cfc413c499a023355db6d0af7
Harish9395 <60750651+Harish9395@users.noreply.github.com>
2026-07-30 03:15:51 +0530

    Update test_health.py

--- automation-framework/api/tests/test_health.py
+++ automation-framework/api/tests/test_health.py
@@ -1,17 +1,15 @@
 import os
-import requests
 import pytest
 
 
 @pytest.mark.api
 @pytest.mark.smoke
-def test_application_health():
+def test_application_health(api_client):
 
     app_url = os.getenv("APP_URL")
 
-    response = requests.get(
-        app_url,
-        timeout=10
+    response = api_client.get(
+        f"{app_url}api/health"
     )
 
     assert response.status_code == 200
