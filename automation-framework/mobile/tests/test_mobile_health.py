2ef7377c44a5b39eb861d48277351c75b2cd22d7
Harish9395 <60750651+Harish9395@users.noreply.github.com>
2026-07-30 03:39:18 +0530

    Update test_mobile_health.py

--- automation-framework/mobile/tests/test_mobile_health.py
+++ automation-framework/mobile/tests/test_mobile_health.py
@@ -1,10 +1,21 @@
+import os
+
 import pytest
 
 
 @pytest.mark.mobile
 @pytest.mark.smoke
-def test_mobile_application_launch(
-    mobile_driver
-):
+def test_mobile_application_launch(mobile_driver):
+
+    app_url = os.getenv("APP_URL")
+
+    if not app_url:
+        raise Exception("APP_URL environment variable is missing")
+
+    mobile_driver.get(
+        f"{app_url.rstrip('/')}/mobile"
+    )
 
     assert mobile_driver.session_id is not None
+
+    assert "ECS Task Mobile App" in mobile_driver.page_source
